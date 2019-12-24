03f86c0d64f1ad22dbb9304bd18a4b0ec36809ef
M6985 <sisian.wu@colatour.com.tw>
2019-12-24 08:44:48 +0800

    團體旅遊名稱修正 , memberIndexVC 、baseProtocol 資料夾更變

---
+++ ColatourB2B/UI/GroupTour/GropeTourPresenter.swift
@@ -0,0 +1,45 @@
+//
+//  GropeTourPresenter.swift
+//  ColatourB2B
+//
+//  Created by M6985 on 2019/12/23.
+//  Copyright © 2019 Colatour. All rights reserved.
+//
+
+import RxSwift
+
+class GropeTourPresenter: GropeTourPresenterProtocol {
+    
+    weak var delegate: GropeTourViewProtocol?
+    fileprivate var dispose = DisposeBag()
+    let accountRepositouy = AccountRepository.shared
+    
+    convenience init(delegate: GropeTourViewProtocol) {
+        self.init()
+        self.delegate = delegate
+    }
+    
+    func getApiToken(){
+        self.delegate?.onStartLoadingHandle(handleType: .ignore)
+        accountRepositouy.apiToken.subscribe(onSuccess: { (model) in
+            self.delegate?.onBindApiTokenComplete()
+            self.delegate?.onCompletedLoadingHandle()
+        }, onError: { (error) in
+            self.delegate?.onBindApiTokenComplete()
+            self.delegate?.onCompletedLoadingHandle()
+        }).disposed(by: dispose)
+    }
+    
+    func getVersionRule() {
+        self.delegate?.onStartLoadingHandle(handleType: .ignore)
+        
+        accountRepositouy.getVersionRule().subscribe(onSuccess: { (model) in
+            self.delegate?.onBindVersionRule(versionRule: model)
+            self.delegate?.onCompletedLoadingHandle()
+        }, onError: { (error) in
+            self.delegate?.onApiErrorHandle(apiError: error as! APIError, handleType: .coverPlate)
+            self.delegate?.onCompletedLoadingHandle()
+        }).disposed(by: dispose)
+    }
+    
+}
