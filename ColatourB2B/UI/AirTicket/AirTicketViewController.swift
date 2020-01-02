2912b3d33d33c1f0cfbeb27b2f59b4a5239347e1
M6985 <sisian.wu@colatour.com.tw>
2020-01-02 11:28:20 +0800

    新增 機票頁

---
+++ ColatourB2B/UI/AirTicket/AirTicketViewController.swift
@@ -0,0 +1,106 @@
+//
+//  AirTicketViewController.swift
+//  ColatourB2B
+//
+//  Created by M6985 on 2019/12/20.
+//  Copyright © 2019 Colatour. All rights reserved.
+//
+
+import UIKit
+
+class AirTicketViewController: BaseViewController {
+
+    @IBOutlet weak var grayBlurView: UIView!
+    @IBOutlet weak var airSearchView: UIView!
+    
+    let transiton = GroupSlideInTransition()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setIsNavShadowEnable(false)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        setNavIcon()
+    }
+
+    private func setNavIcon(){
+        self.setNavTitle(title: "機票")
+        
+        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.init(named: "通用綠")!]
+        
+        let contaceButtonView = UIButton(type: .system)
+        
+        let rightImage = #imageLiteral(resourceName: "home_contavt")
+        contaceButtonView.setImage(rightImage, for: .normal)
+        contaceButtonView.addTarget(self, action: #selector(self.onTouchContact), for: .touchUpInside)
+
+        var contaceBarButtonItem = UIBarButtonItem(customView: contaceButtonView)
+        contaceBarButtonItem = UIBarButtonItem(image: rightImage, style: .plain, target: self, action: #selector(self.onTouchContact))
+        
+        let menuButtonView = UIButton(type: .system)
+        
+        let leftImage = #imageLiteral(resourceName: "home_menu")
+        menuButtonView.setImage(leftImage, for: .normal)
+        menuButtonView.addTarget(self, action: #selector(self.onTouchMenu), for: .touchUpInside)
+
+        var menuBarButtonItem = UIBarButtonItem(customView: menuButtonView)
+        menuBarButtonItem = UIBarButtonItem(image: leftImage, style: .plain, target: self, action: #selector(self.onTouchMenu))
+        
+        self.navigationItem.leftBarButtonItem = menuBarButtonItem
+        self.navigationItem.rightBarButtonItem = contaceBarButtonItem
+    }
+    
+    @objc func onTouchMenu (){
+        let vc = getVC(st: "GroupTour", vc: "GroupSliderViewController") as! GroupSliderViewController
+        //vc.delegate = self
+        vc.modalPresentationStyle = .overCurrentContext
+        vc.transitioningDelegate = self
+        vc.setVC(serverList: [])
+        present(vc, animated: true)
+    }
+    
+    @objc func onTouchContact (){
+        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        /*self.result?.contactList.forEach({ (server) in
+            alert.addAction(UIAlertAction(title: server.linkName , style: .default, handler: { (_) in
+                self.handleLinkType(linkType: server.linkType, linkValue: server.linkValue, linkText: nil)
+            }))
+        })*/
+
+        alert.addAction(UIAlertAction(title: "取消", style: .destructive))
+
+        self.present(alert, animated: true)
+    }
+    
+    private func setSearchGes(){
+        let ges = UITapGestureRecognizer(target: self, action: #selector(onTouchSearch))
+        self.airSearchView.addGestureRecognizer(ges)
+        self.airSearchView.isUserInteractionEnabled = true
+    }
+    
+    @objc private func onTouchSearch(){
+        ()
+    }
+}
+extension AirTicketViewController: UIViewControllerTransitioningDelegate {
+    func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        transiton.isPresenting = true
+        grayBlurView.isHidden = false
+        self.tabBarController?.tabBar.isHidden = true
+        
+        return transiton
+    }
+
+    func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        transiton.isPresenting = false
+        self.tabBarController?.tabBar.isHidden = false
+        grayBlurView.isHidden = true
+        
+        self.setTabBarType(tabBarType: .notHidden)
+        return transiton
+    }
+}
