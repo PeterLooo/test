871831b3c5ee9839db264af86eb5e7255591f17e
M6985 <sisian.wu@colatour.com.tw>
2020-01-03 16:01:32 +0800

    新增 linkType 事件 取得 WebUrl Open WebView

--- ColatourB2B/UI/Common/BaseProtocol.swift
+++ ColatourB2B/UI/Common/BaseProtocol.swift
@@ -14,6 +14,7 @@ protocol BaseViewProtocol: NSObjectProtocol {
     func onCompletedLoadingHandle()
     func loadData()
     func onBindAccessToken(response: AccessTokenResponse)
+    func onBindAccessWebUrl(url: String)
 }
 
 protocol BasePresenterProtocol {
