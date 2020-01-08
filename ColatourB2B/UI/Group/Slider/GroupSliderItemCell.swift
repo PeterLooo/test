bd4d1f2cf7ec8960f595d39d3f680868e98d052b
M6985 <sisian.wu@colatour.com.tw>
2020-01-08 16:07:59 +0800

    修正 工具列顯示樣式

--- ColatourB2B/UI/Group/Slider/GroupSliderItemCell.swift
+++ ColatourB2B/UI/Group/Slider/GroupSliderItemCell.swift
@@ -15,6 +15,7 @@ class GroupSliderItemCell: UITableViewCell {
     @IBOutlet weak var serverTitle: UILabel!
     @IBOutlet weak var titleTopConstraint: NSLayoutConstraint!
     @IBOutlet weak var bottomViewHeight: NSLayoutConstraint!
+    @IBOutlet weak var titleBottomConstraint: NSLayoutConstraint!
     
     weak var delegate: GroupSliderItemCellProtocol?
     private var serverData: ServerData?
@@ -25,12 +26,16 @@ class GroupSliderItemCell: UITableViewCell {
         self.isUserInteractionEnabled = true
     }
 
-    func setCell(serverData: ServerData, isNeedLine: Bool, isFirst: Bool){
+    func setCell(serverData: ServerData,
+                 isNeedLine: Bool,
+                 isFirst: Bool,
+                 isLast: Bool){
+        
         self.serverData = serverData
         self.serverTitle.text = serverData.linkName
         titleTopConstraint.constant = isFirst ? 26 : 12
         bottomViewHeight.constant = isNeedLine ? 1 : 0
-        
+        titleBottomConstraint.constant = isLast ? 26 : 12
     }
     
     @objc func onTouchDate(){
