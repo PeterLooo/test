4181149afd1106707d58d17b92d6b9d0893953cd
M6985 <sisian.wu@colatour.com.tw>
2020-02-18 14:53:26 +0800

    新增 eDM_Source 欄位 , 修正 通知 cell 樣式

--- ColatourB2B/Response/Notification/NoticeResponse.swift
+++ ColatourB2B/Response/Notification/NoticeResponse.swift
@@ -92,7 +92,7 @@ class NotiItem: NSObject {
         self.notiTitle = notiTitle
         self.notiContent = notiContent
         self.notiId = notiId
-        self.notiDate = FormatUtil.convertStringToString(dateStringFrom: notiDate!, dateFormatTo: "MM/dd HH:mm")
+        self.notiDate = FormatUtil.convertStringToString(dateStringFrom: notiDate!, dateFormatTo: "MM/dd")
         self.unreadMark = unreadMark
         self.linkType = linkType
         self.linkValue = linkValue
