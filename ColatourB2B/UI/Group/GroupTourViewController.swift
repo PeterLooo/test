dd4026a33db01891e0982fe43446b610b299e67e
M6985 <sisian.wu@colatour.com.tw>
2020-02-26 15:22:48 +0800

    新增 全域變數 cancelLoadData

--- ColatourB2B/UI/Group/GroupTourViewController.swift
+++ ColatourB2B/UI/Group/GroupTourViewController.swift
@@ -43,8 +43,12 @@ class GroupTourViewController: BaseViewController {
         setNavIcon()
         setUpTableView()
         setSearchView()
-        
-        loadData()
+        // 為了不讓點推播重複去要 accessToken 先判斷 cancelLoadData
+        if cancelLoadData == true {
+            cancelLoadData = false
+        }else{
+            loadData()
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
