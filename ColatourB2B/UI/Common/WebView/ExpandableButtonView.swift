e142e4ef5737875b9079c0e25575eee43f55a9ec
M7635 <sharon.chiu@colatour.com.tw>
2020-02-27 17:40:01 +0800

    新增 展開式按鈕 圖片

---
+++ ColatourB2B/UI/Common/WebView/ExpandableButtonView.swift
@@ -0,0 +1,21 @@
+//
+//  ExpandableButtonView.swift
+//  ColatourB2B
+//
+//  Created by 7635 邱郁雯 on 2020/2/27.
+//  Copyright © 2020 Colatour. All rights reserved.
+//
+
+import UIKit
+
+class ExpandableButtonView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
