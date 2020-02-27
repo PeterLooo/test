ecaa0905d750f95e94d6020c9693f70d0848b008
M7635 <sharon.chiu@colatour.com.tw>
2020-02-27 17:42:30 +0800

    刪除測試資料

--- ColatourB2B/API/APIManager.swift
+++ ColatourB2B/API/APIManager.swift
@@ -360,7 +360,7 @@ extension APIManager {
     
     func getWebViewTourShareList(tourCode:String,tourDate:String) -> Single<[String:Any]> {
         var appendUrl = ""
-        appendUrl = "?Tour_Code=HDS032705BR0B&Tour_Date=2020/03/27"//"?Tour_Code=\(tourCode)&Tour_Date=\(tourDate)"
+        appendUrl = "?Tour_Code=\(tourCode)&Tour_Date=\(tourDate)"
         return manager(method: .get, appendUrl: appendUrl, url: APIUrl.portalApi(type: .tourShare), parameters: nil, appendHeaders: nil)
     }
 }
