491316fe8565ae06d4f1ca5722e78bf2becc467f
M6985 <sisian.wu@colatour.com.tw>
2020-03-09 10:22:01 +0800

    新增 測試 Response

---
+++ ColatourB2B/UI/AirTicket/TKTSearch/AirTicketSearchViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AirTicketSearchViewController.swift
+//  ColatourB2B
+//
+//  Created by 6985 吳思賢 on 2020/3/9.
+//  Copyright © 2020 Colatour. All rights reserved.
+//
+
+import UIKit
+
+class AirTicketSearchViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
