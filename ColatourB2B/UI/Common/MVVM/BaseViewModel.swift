a503cef2ca533a0fdb1c69201221a95a758620ef
M6985 <sisian.wu@colatour.com.tw>
2021-06-25 09:48:28 +0800

    修正 Base

--- ColatourB2B/UI/Common/MVVM/BaseViewModel.swift
+++ ColatourB2B/UI/Common/MVVM/BaseViewModel.swift
@@ -10,12 +10,40 @@ import Foundation
 import RxSwift
 
 class BaseViewModel {
+    
+    private let dispose = DisposeBag()
+    
     var onStartLoadingHandle: ((APILoadingHandleType) -> ())?
-    var onCompletedLoadingHandle: (() -> ())?
     var onApiErrorHandle: ((APIError,APIErrorHandleType) -> ())?
     var handleLinkType: ((_ linkType: LinkType, _ linkValue: String?, _ linkText: String?, _ source: String? ) -> ())?
-    var loadData: (() -> ())?
     var onBindAccessWebUrl: ((_ url: String,_ title: String,_ openBrowserOrAppWebView: OpenBrowserOrAppWebView)->())?
     var onBindAccessTokenByLink:((_ linkType: LinkType,_ linkValue: String?)->())?
+    var loadData: (() -> ())?
+    var onCompletedLoadingHandle: (() -> ())?
     
+    func getAccessToken(linkType: LinkType?, linkValue: String?){
+        self.onStartLoadingHandle?(.ignore)
+        AccountRepository.shared.getAccessToken(getLocalToken:false).subscribe(onSuccess: { [weak self] _ in
+            
+            if linkType != nil {
+                self?.onBindAccessTokenByLink?(linkType!, linkValue)
+            }
+            self?.loadData?()
+            self?.onCompletedLoadingHandle?()
+        }, onError: { [weak self] (error) in
+            self?.onApiErrorHandle?(error as! APIError, .coverPlate)
+            self?.onCompletedLoadingHandle?()
+        }).disposed(by: dispose)
+    }
+    
+    func getAccessWebUrl(webUrl:String, title: String, openBrowserOrAppWebView: OpenBrowserOrAppWebView) {
+        self.onStartLoadingHandle?(.ignore)
+        AccountRepository.shared.getAccessWeb(webUrl: webUrl).subscribe(onSuccess: { [weak self] (url) in
+            self?.onBindAccessWebUrl?(url, title, openBrowserOrAppWebView)
+            self?.onCompletedLoadingHandle?()
+        }, onError: { [weak self] (error) in
+            self?.onApiErrorHandle?(error as! APIError, .coverPlate)
+            self?.onCompletedLoadingHandle?()
+        }).disposed(by: dispose)
+    }
 }
--- ColatourB2B/UI/Common/MVVM/BaseViewModelPrivate.swift
+++ ColatourB2B/UI/Common/MVVM/BaseViewModelPrivate.swift
@@ -14,34 +14,6 @@ class BaseViewModelPrivate: BaseViewModel {
     static let share = BaseViewModelPrivate()
     
     private var dispose = DisposeBag()
-    override init() {
-        print("BaseViewModelPrivate didInit")
-    }
-    func getAccessToken(linkType: LinkType?, linkValue: String?){
-        self.onStartLoadingHandle?(.ignore)
-        
-        AccountRepository.shared.getAccessToken(getLocalToken:false).subscribe(onSuccess: { [weak self ](_) in
-            
-            if linkType != nil {
-                self?.onBindAccessTokenByLink?(linkType!, linkValue)
-            }
-            self?.loadData?()
-            self?.onCompletedLoadingHandle?()
-        }, onError: { [weak self] (error) in
-            self?.onApiErrorHandle?(error as! APIError, .coverPlate)
-            self?.onCompletedLoadingHandle?()
-        }).disposed(by: dispose)
-    }
     
-    func getAccessWebUrl(webUrl:String, title: String, openBrowserOrAppWebView: OpenBrowserOrAppWebView) {
-        
-        self.onStartLoadingHandle?(.ignore)
-        AccountRepository.shared.getAccessWeb(webUrl: webUrl).subscribe(onSuccess: { [weak self] (url) in
-            self?.onBindAccessWebUrl?(url, title, openBrowserOrAppWebView)
-            self?.onCompletedLoadingHandle?()
-        }, onError: { [weak self] (error) in
-            self?.onApiErrorHandle?(error as! APIError, .coverPlate)
-            self?.onCompletedLoadingHandle?()
-        }).disposed(by: dispose)
-    }
+    
 }
